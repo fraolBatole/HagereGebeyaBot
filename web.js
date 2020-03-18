26f2ab020fd5af20d7d72b8038a8cd4516224f30
fraolBatole <60069363+fraolBatole@users.noreply.github.com>
2020-03-17 19:59:33 -0500

    Update web.js

--- web.js
+++ web.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const packageInfo = require('./package.json');
-const Telegraf = require('telegraf')
-const botNew = new Telegraf(token)
-botNew.start((ctx) => ctx.reply('Welcome'))
-botNew.help((ctx) => ctx.reply('Send me a sticker'))
-botNew.on('sticker', (ctx) => ctx.reply('👍'))
-botNew.hears('hi', (ctx) => ctx.reply('Hey there'))
-botNew.launch()
+
 
 const app = express();
 app.use(bodyParser.json());
