093beec437ebf9c33db49929a248ef1e2cd51a67
Fraol Batole <fraol@Fraols-MacBook-Air.local>
2020-03-17 19:54:23 -0500

    ddd

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ require("dotenv").config();
 
 var bot = require('./bot');
 require('./web')(bot);
+
+const Telegraf = require('telegraf')
+
+const botNew = new Telegraf(token)
+botNew.start((ctx) => ctx.reply('Welcome'))
+botNew.help((ctx) => ctx.reply('Send me a sticker'))
+botNew.on('sticker', (ctx) => ctx.reply('👍'))
+botNew.hears('hi', (ctx) => ctx.reply('Hey there'))
+botNew.launch()
